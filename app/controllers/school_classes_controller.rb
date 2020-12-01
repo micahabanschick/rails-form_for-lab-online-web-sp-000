25162b9301c264862f0585b7d21f040f3a06dd39
Micah Banschick <micha.ban@gmail.com>
2020-12-01 22:35:05 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,15 +5,15 @@ class SchoolClassesController < ActionController::Base
   end
 
   def show
-    @school_class = school_class.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def new
-    @school_class = school_class.new
+    @school_class = SchoolClass.new
   end
 
   def create
-    @school_class = school_class.new
+    @school_class = SchoolClass.new
     @school_class.title = params[:title]
     @school_class.description = params[:description]
     @school_class.save
@@ -22,11 +22,11 @@ class SchoolClassesController < ActionController::Base
 
   # add edit and update methods here
   def edit
-    @school_class = school_class.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
-    @school_class = school_class.find(params[:id])
+    @school_class = SchoolClass.find(params[:id])
     @school_class.update(title: params[:school_class][:title], description: params[:school_class][:description])
     redirect_to school_class_path(@school_class)
   end
