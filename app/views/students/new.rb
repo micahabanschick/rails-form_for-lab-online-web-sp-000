4d62742e3b210f78970a318e7bcb55453f94270a
Micah Banschick <micha.ban@gmail.com>
2020-12-01 22:42:44 +0000

    Automatically backed up by Learn

--- app/views/students/new.rb
+++ app/views/students/new.rb
@@ -1,9 +1,9 @@
-<%= form_tag articles_path do %>
-  <label>Article title:</label><br>
+<%= form_for students_path do %>
+  <label>student title:</label><br>
   <%= text_field_tag :title %><br>
 
-  <label>Article Description</label><br>
+  <label>student Description</label><br>
   <%= text_area_tag :description %><br>
 
-  <%= submit_tag "Submit Article" %>
+  <%= submit_tag "Submit student" %>
 <% end %>
