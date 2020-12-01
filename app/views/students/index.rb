3cbf15beadc6402039286e7404851b38c0f78e7d
Micah Banschick <micha.ban@gmail.com>
2020-12-01 22:40:21 +0000

    Automatically backed up by Learn

--- app/views/students/index.rb
+++ app/views/students/index.rb
@@ -1,3 +1,5 @@
-<% @articles.each do |article| %>
+<% @students.each do |student| %>
+  <div><%= student.first_name %></div>
+  <div><%= student.last_name %></div>
   <div><%= article.title %></div>
 <% end %>
