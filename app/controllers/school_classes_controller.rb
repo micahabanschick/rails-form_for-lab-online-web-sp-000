0225323df089ef60b7fdbe4433366815d86e980c
Micah Banschick <micha.ban@gmail.com>
2020-12-01 22:22:59 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,34 @@
 class SchoolClassesController < ActionController::Base
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.title = params[:title]
+    @article.description = params[:description]
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end
+  
 end
